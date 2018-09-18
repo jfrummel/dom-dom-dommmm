16838e818e1cd5ab6c47698c6d24fbcb71d36f54
unknown <jfrummel@gmail.com>
2018-09-18 17:46:29 -0500

    Finished with some help

--- scripts.js
+++ scripts.js
@@ -5,8 +5,7 @@ document.body.appendChild(button);
 let container = document.createElement('div');
 container.className = 'container';
 let num = 0;
-let colors = ['red', 'blue', 'green', 'purple', 'pink', 'yellow', 'orange'];
-let randomColor = colors[Math.floor(Math.random()*colors.length)];
+
 button.addEventListener('click', function() {
     num++;
     let div = document.createElement('div');
@@ -15,15 +14,34 @@ button.addEventListener('click', function() {
     container.appendChild(div);
     document.body.appendChild(container);
     div.addEventListener('click', function() {
+        let colors = ['red', 'blue', 'green', 'purple', 'pink', 'yellow', 'orange'];
+        let randomColor = colors[Math.floor(Math.random()*colors.length)];
         div.style.backgroundColor = randomColor;
     });
     div.addEventListener('mouseover', function() {
-    div.innerHTML = div.id
+        div.innerHTML = div.id
     });
     div.addEventListener('mouseleave', function() {
         div.innerHTML = " ";
     });
-    
+    div.addEventListener('dblclick', function() {
+        let len = document.getElementsByClassName('box').length;
+        let nextSquare = document.getElementById(div.id).nextElementSibling;
+        let previousSquare = document.getElementById(div.id).previousElementSibling;
+         if (div.id % 2 == 0) {
+            if (div.id >= len) {
+                alert("There are no boxes after this one!");
+            } else {
+                nextSquare.parentNode.removeChild(nextSquare); 
+            }
+        } else {
+            if (previousSquare == null) {
+                alert("There are no boxes before this one!");
+            } else {
+                previousSquare.parentNode.removeChild(previousSquare);
+            }
+        }   
+    }); 
 });
 
 
